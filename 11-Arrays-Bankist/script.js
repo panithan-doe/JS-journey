916c1098e776de5f4c84903157861cdc4b429f2f
Panithan <doedoserpc@gmail.com>
2024-06-07 15:16:37 +0700

    create displayMovement function

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -61,39 +61,87 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// ---------------------------------------------
+// display movements
+const displayMovement = function (movements) {
+  containerMovements.innerHTML = ''; // set text to empty
+  // NOTE: like textContent but innerHTML is including HTML tags, textContent just text itself
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovement(account1.movements);
+
+// ---------------------------------------------------
+
+// const displayMovement = function(movements) {
+  
+//   containerMovements.innerHTML = '';
+
+//   movements.forEach(function(movement, i) {
+//     const type = movement > 0 ? 'deposit' : 'withdrawal';
+    
+//     const html = `
+//     <div class="movements__row">
+//       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+//       <div class="movements__value">${movement}</div>
+//     </div>
+//     `
+
+//     containerMovements.insertAdjacentHTML('afterbegin', html)
+//   })
+
+// }
+
+
+// displayMovement(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for of
-for (const movement of movements) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}.`);
-  } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
-  }
-}
+// // for of
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}.`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// }
 
-// for each
-movements.forEach(function(movement) {
-  console.log(movement + 1000000);
-})
+// // for each
+// movements.forEach(function (movement) {
+//   console.log(movement + 1000000);
+// });
 
-movements.forEach(function(mov, i, arr) {
-  console.log(mov, i, arr);
-})
+// movements.forEach(function (mov, i, arr) {
+//   console.log(mov, i, arr);
+// });
 
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
 
-console.log(currenciesUnique);
+// console.log(currenciesUnique);
 
 /////////////////////////////////////////////////
 
@@ -101,8 +149,8 @@ console.log(currenciesUnique);
 // let arr = ['a', 'b', 'c', 'd', 'e']
 
 // // slice
-// console.log(arr.slice(2));      
-// console.log(arr.slice(2, 4));  
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
 // console.log(arr.slice(-2));
 
 // // join
